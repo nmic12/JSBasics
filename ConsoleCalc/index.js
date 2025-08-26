fa1655c7c093e6455b7e0605c87395dc9d0adfd0
Nícolas Michels <nicolas.michels12@gmail.com>
2025-08-26 19:57:12 -0300

    prompt-sync

--- ConsoleCalc/index.js
+++ ConsoleCalc/index.js
@@ -1,24 +1,5 @@
 const prompt = require("prompt-sync")();
 
-
-// while(true){
-//     number1 = parseFloat(prompt("Please enter the first number: ")); // parseFloat cast a string pra float
-//     if(isNaN(number1)) {
-//         console.log("Invalid input")
-//     } else {
-//         break;
-//     }
-// }
-
-// while(true){
-//     number2 = parseFloat(prompt("Please enter the second number: ")); // parseFloat cast a string pra float
-//     if(isNaN(number2)) {
-//         console.log("Invalid input")
-//     } else {
-//         break;
-//     }
-// }
-
 function validateNumbers(numberString) {
     while(true){
         const number = parseFloat(prompt("Enter " + numberString + " number: " )); // parseFloat cast a string pra float
@@ -33,7 +14,6 @@ function validateNumbers(numberString) {
 const number1 = validateNumbers("first");
 const number2 = validateNumbers("second");
 
-//const number2 = parseFloat(prompt("Please enter the second number: "));
 const operator = prompt("Please enter the operator: ");
 
 let result;
---
+++ UserInput/index.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")()
+const name = prompt("Whats your name? ") // user input
+console.log("Hello " + name + " how are you? ");
